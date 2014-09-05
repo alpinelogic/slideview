5485169a274aae35f7377c9eea386b1cc27b7925
Billy Onjea <vasili.onjea@gmail.com>
2014-09-04 20:34:31 -0400

    simplifying file separation

---
+++ src/settings.js
@@ -0,0 +1,22 @@
+  // Constants
+  var SELECTORS = {
+    plugin: '.slideview',
+    slide: '.slide',
+    offscreen: '.offscreen',
+    nav: '.nav',
+    prev: '.prev',
+    next: '.next'
+  };  
+
+  Slideview.version = '{{@VERSION}}';
+
+  // Default options.
+  Slideview.defaults = {
+    slidesToShow: 4,
+
+    // when `true` it slides back to the 1st slide when reaching the end
+    endSlideBack: false,
+
+    // only for Browsers that may have rounding errors
+    resizeDelay: 150
+  };
